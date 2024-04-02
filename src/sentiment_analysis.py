b32783ae1cbaca96344deee3e9ec9b9a8cdb03f2
AnilaGToor <anila@infoscience.ai>
2024-04-02 09:45:04 +0500

    created src folder

---
+++ src/sentiment_analysis.py
@@ -0,0 +1,27 @@
+import requests
+import streamlit as st
+
+def sentiment_analysis(text):
+    try:
+        API_URL = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest"
+        headers = {"Authorization": f"Bearer {Access_Token}"}
+
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+
+        output = query({
+            "inputs": text,
+        })
+    except requests.ConnectionError as e:
+        st.error("Connection error")
+    except requests.ConnectTimeout as e:
+        st.error("Connection timeout")
+    except requests.RequestException as e:
+        st.error("Request exception")
+    except (Exception, ValueError) as e:
+        st.error("Unknown error")
+    except requests.HTTPError as e:
+        st.error("HTTP error")
+    else:
+        return output[0][0]['label'].title()
